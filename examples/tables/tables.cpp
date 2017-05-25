35eea1af20322b59ddee877cc48d61eac1c345f0
Nigel Atkinson <nigel@atkinson.kiwi>
2017-05-25 14:14:08 +1000

    We don't need stackDump in this example

--- examples/tables/tables.cpp
+++ examples/tables/tables.cpp
@@ -7,30 +7,6 @@
 using std::cout;
 using std::endl;
 
-static void stackDump (lua_State *L) {
-    int i=lua_gettop(L);
-    cout << " ----------------  Stack Dump ----------------\n";
-    while(  i   ) {
-        int t = lua_type(L, i);
-        switch (t) {
-            case LUA_TSTRING:
-                printf("%d: “%s”\n", i, lua_tostring(L, i));
-                break;
-            case LUA_TBOOLEAN:
-                printf("%d: %s\n",i,lua_toboolean(L, i) ? "true" : "false");
-                break;
-            case LUA_TNUMBER:
-                printf("%d: %g\n",  i, lua_tonumber(L, i));
-                break;
-            default: 
-                printf("%d: %s 0x%x\n", i, lua_typename(L, t), lua_topointer(L, i)); 
-                break;
-        }
-        i--;
-    }
-    cout << "--------------- Stack Dump Finished ---------------\n";
-}
-
 void run( lua_State *L, const char *code )
 {
     cout << "code> " << code << endl;
