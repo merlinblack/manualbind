f508d4874e686c5eb2a5cea3fc30a6cc310bbd75
Nigel Atkinson <merlinblack@users.noreply.github.com>
2017-05-30 15:35:11 +1000

    Old LuaRef is now old, new is now LuaRef.
    Still some things to add perhaps.

--- examples/luaref/luaref.cpp
+++ examples/luaref/luaref.cpp
@@ -4,8 +4,33 @@
 
 using namespace std;
 
+static void dump (lua_State *L) {
+    int i=lua_gettop(L);
+    cout << " ----------------  Stack Dump ----------------\n";
+    while(  i   ) {
+        int t = lua_type(L, i);
+        switch (t) {
+            case LUA_TSTRING:
+                printf("%d: “%s”\n", i, lua_tostring(L, i));
+                break;
+            case LUA_TBOOLEAN:
+                printf("%d: %s\n",i,lua_toboolean(L, i) ? "true" : "false");
+                break;
+            case LUA_TNUMBER:
+                printf("%d: %g\n",  i, lua_tonumber(L, i));
+                break;
+            default:
+                printf("%d: %s 0x%x\n", i, lua_typename(L, t), lua_topointer(L, i));
+                break;
+        }
+        i--;
+    }
+    cout << "--------------- Stack Dump Finished ---------------\n";
+}
+
 void run( lua_State* L, const char* code )
 {
+    cout << "code: " << code << endl;
     if( luaL_dostring( L, code ) )
     {
         cout << lua_tostring( L, -1 ) << endl;
@@ -13,6 +38,11 @@ void run( lua_State* L, const char* code )
     }
 }
 
+LuaRef getTesting( lua_State* L )
+{
+    lua_getglobal( L, "testing" );
+    return LuaRef::fromStack(L);
+}
 
 int main()
 {
@@ -22,28 +52,63 @@ int main()
     run( L, "function testing( ... ) print( '> ', ... ) end" );
 
     {
-        LuaRef testing = getGlobal( L, "testing" );
-        LuaRef tbl = newTable( L );
-        tbl["testing"] = testing;
-        tbl.push(L);
+        LuaRef testing = LuaRef::getGlobal( L, "testing" );
+        LuaRef table = LuaRef::newTable( L );
+        table["testing"] = testing;
+
+        table.push();
         lua_setglobal( L, "a" );
-        
+
+        run( L, "print( a.testing )" );
+        run( L, "a.b = {}" );
+        run( L, "a.b.c = {}" );
+
+        cout << "Is table a table? " 
+             << (table.isTable() ? "true" : "false" )
+             << endl;
+        cout << "Is table[\"b\"] a table? " 
+             << (table["b"].isTable() ? "true" : "false" ) 
+             << endl;
+
+        table["b"]["c"]["hello"] = "World!";
+
+        run( L, "print( a.b.c.hello )" );
+
+        table["b"][10] = "Index 10";
+
+        run( L, "print( a.b[10] )" );
+
+        table["b"] = LuaNil();
+
+        run( L, "print( a.b )" );
 
         testing();
         testing( 1, 2, 3 );
         testing( "Hello" );
         testing( "Hello", "World" );
+        
+        // Note: BAD. This makes a boat load of LuaRef copies
+        // temporarily. The recursive parameter pushing
+        // means later parameters get for each parameter in
+        // front of them.
+        // So put expensive parameters at the start!!!
         testing( "Hello", "World", 1, 2, 3, testing );
 
         testing( "Nigel", "Alara", "Aldora", "Ayna",
-                "Sarah", "Gavin", "Joe",    "Linda",
-                "Tom",   "Sonja", "Greg",   "Trish" 
+                 "Sarah", "Gavin", "Joe",    "Linda",
+                 "Tom",   "Sonja", "Greg",   "Trish" 
                );
 
-        testing = getGlobal( L, "a" );
+        table["testing"](testing,3,2,1,"Calling array element");
+        table["testing"]();
 
-        testing["testing"](testing,3,2,1,"Calling array element");
-        testing["testing"]();
+        LuaRef newfuncref( L );
+
+        newfuncref = testing;
+
+        newfuncref( "Did it copy correctly?" );
+
+        newfuncref = getTesting( L );
     }
 
     lua_close(L);
