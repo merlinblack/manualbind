874d8394256551cf2198f02a9e8b85d84b7743a6
Nigel Atkinson <merlinblack@users.noreply.github.com>
2017-06-22 16:46:39 +1000

    Makefile update

--- examples/config.mk
+++ examples/config.mk
@@ -1,4 +1,5 @@
 # Local install of Lua
 # -std=c++11 needed for Xcode g++
-CFLAGS=-std=c++11 -I/usr/local/include
-LFLAGS=-L/usr/local/lib
+#CFLAGS=-std=c++11 -I/usr/local/include -O3
+#LFLAGS=-L/usr/local/lib
+CFLAGS=-O3
