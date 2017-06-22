6c61cb6cacf8169cad26fde4aaa584522e34f5d1
Nigel Atkinson <nigel@atkinson.gen.nz>
2017-06-22 19:25:08 +1000

    Make makefile detect MacOS

--- examples/config.mk
+++ examples/config.mk
@@ -1,5 +1,9 @@
-# Local install of Lua
 # -std=c++11 needed for Xcode g++
-#CFLAGS=-std=c++11 -I/usr/local/include -O3
-#LFLAGS=-L/usr/local/lib
-CFLAGS=-O3
+UNAME_S:=$(shell uname -s)
+ifeq ($(UNAME_S),Darwin)
+	CFLAGS+=-std=c++11
+endif
+# Local install of Lua
+#CFLAGS+=-I/usr/local/include
+#LFLAGS+=-L/usr/local/lib
+CFLAGS+=-O3
