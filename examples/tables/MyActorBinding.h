ca10ff9f9acf37b301e37ccdabf2f6139a649e58
Nigel Atkinson <nigel@atkinson.kiwi>
2017-06-25 05:04:17 +1000

    Fix name of check args function

--- examples/tables/MyActorBinding.h
+++ examples/tables/MyActorBinding.h
@@ -30,7 +30,7 @@ struct MyActorBinding: public Binding<MyActorBinding, MyActor> {
     {
         std::cout << "Create called\n";
 
-        checkArgCount( L, 1 );
+        LuaBindingCheckArgCount( L, 1 );
 
         const char *name = luaL_checkstring( L, 1 );
 
@@ -45,7 +45,7 @@ struct MyActorBinding: public Binding<MyActorBinding, MyActor> {
 
     static int get_name( lua_State *L )
     {
-        checkArgCount( L, 2 );
+        LuaBindingCheckArgCount( L, 2 );
         MyActorPtr a = fromStack( L, 1 );
         lua_pushstring( L, a->_name.c_str() );
         return 1;
