494c3bfec878bac08b6ce170fcb4e5363b4ed468
Nigel Atkinson <nigel@atkinson.kiwi>
2017-08-01 16:57:04 +1000

    Always use -std=c++11 for YCM

--- examples/config.mk
+++ examples/config.mk
@@ -1,11 +1,11 @@
 # -std=c++11 needed for Xcode g++
 UNAME_S:=$(shell uname -s)
 ifeq ($(UNAME_S),Darwin)
-	CFLAGS+=-std=c++11
 	LUA=lua5.3
 else
 	LUA=lua
 endif
 CFLAGS+=$(shell pkg-config --cflags $(LUA) )
 LFLAGS+=$(shell pkg-config --libs   $(LUA) )
+CFLAGS+=-std=c++11
 CFLAGS+=-O3 -Wall
