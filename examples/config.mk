3879aa3d1f163f24974c14c21dd15ec3dcc45277
Nigel Atkinson <nigel@atkinson.kiwi>
2017-08-02 11:06:34 +1000

    Refine Makefiles
    Makes it easier to use clang++ when I want to.

--- examples/config.mk
+++ examples/config.mk
@@ -1,11 +1,10 @@
-# -std=c++11 needed for Xcode g++
+# Lua pkg-config name is different under MacOS brew
 UNAME_S:=$(shell uname -s)
 ifeq ($(UNAME_S),Darwin)
 	LUA=lua5.3
 else
 	LUA=lua
 endif
-CFLAGS+=$(shell pkg-config --cflags $(LUA) )
-LFLAGS+=$(shell pkg-config --libs   $(LUA) )
-CFLAGS+=-std=c++11
-CFLAGS+=-O3 -Wall
+CXXFLAGS+=$(shell pkg-config --cflags $(LUA) )
+LDFLAGS+=$(shell pkg-config --libs   $(LUA) )
+CXXFLAGS+=-std=c++11 -O3 -Wall
