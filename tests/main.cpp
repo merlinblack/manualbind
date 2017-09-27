eab51a7e6b800f1aa3512535614c61dda3018952
Nigel Atkinson <nigel@atkinson.gen.nz>
2017-09-27 23:23:42 +1000

    Properties test case
    And ycm completion generation working on Mac & (still) Linux.

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,2 +1,13 @@
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
+#include <lua.hpp>
+
+void run( lua_State* L, const char* code )
+{
+    if( luaL_dostring( L, code ) )
+    {
+        WARN( lua_tostring( L, -1 ) );
+        lua_pop( L, 1 );
+    }
+}
+
