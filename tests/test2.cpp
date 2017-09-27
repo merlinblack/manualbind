5befd50ed36d85d436f6b90afcd0e05faae9fce6
Nigel Atkinson <merlinblack@users.noreply.github.com>
2017-09-27 16:01:56 +1000

    Beginnings of unit testing using Catch
    https://github.com/philsquared/Catch

---
+++ tests/test2.cpp
@@ -0,0 +1,80 @@
+#include "catch.hpp"
+#include "LuaBinding.h"
+
+using namespace ManualBind;
+
+bool gTest2DestructorCalled;
+
+class Basic
+{
+    public:
+    ~Basic()
+    {
+        gTest2DestructorCalled = true;
+    }
+};
+
+// A very basic opaque pointer binding.
+struct BasicBinding : public Binding<BasicBinding,Basic>
+{
+    static constexpr const char* class_name = "Basic";
+
+    static int create( lua_State* L )
+    {
+        return luaL_error( L, "Can not create an instance to opaque pointer" );
+    }
+};
+
+using BasicPtr = std::shared_ptr<Basic>;
+
+TEST_CASE( "Binding maintains shared pointer to same class instance." ) {
+
+    lua_State* L = luaL_newstate();
+
+    BasicBinding::register_class( L );
+
+    BasicPtr bp = std::make_shared<Basic>();
+
+    // Give Lua a copy.
+    BasicBinding::push( L, bp );
+    lua_setglobal( L, "bp" );
+
+    // Retrieve
+    lua_getglobal( L, "bp" );
+    BasicPtr fromLua = BasicBinding::fromStack( L, 1 );
+    lua_pop( L, 1 );
+
+    REQUIRE( bp == fromLua );
+
+    lua_close( L );
+}
+
+TEST_CASE( "Garbage collection calls class destructor." ) {
+
+    lua_State* L = luaL_newstate();
+
+    BasicBinding::register_class( L );
+
+    gTest2DestructorCalled = false;
+
+    {
+        BasicPtr bp = std::make_shared<Basic>();
+
+        // Give Lua a copy.
+        BasicBinding::push( L, bp );
+        lua_setglobal( L, "bp" );
+
+        // bp going out of scope and decrease ref count to 1
+    }
+
+    REQUIRE( gTest2DestructorCalled != true );
+
+    // Garbage collect Lua's pointer.
+    lua_pushnil( L );
+    lua_setglobal( L, "bp" );
+    lua_gc( L, LUA_GCCOLLECT, 0 );
+
+    REQUIRE( gTest2DestructorCalled == true );
+
+    lua_close( L );
+}
