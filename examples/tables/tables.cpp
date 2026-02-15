d2dbdd5c83f6d0a5ebc6b6d4e8128341f1a02a48
Nigel Atkinson <merlinblack@users.noreply.github.com>
2026-02-15 22:54:44 +1100

    Add to tables example

--- examples/tables/tables.cpp
+++ examples/tables/tables.cpp
@@ -1,16 +1,17 @@
 #include <iostream>
+#include <lua.hpp>
+#include <unordered_map>
 #include <vector>
 #include "MyActor.h"
 #include "MyActorBinding.h"
 #include "common.h"
 
 using std::cout;
-using std::endl;
 using std::make_shared;
 
 using MyActorList = std::vector<MyActorPtr>;
 
-MyActorList createList()
+MyActorList createActorList()
 {
   MyActorList actors{
       make_shared<MyActor>("James"), make_shared<MyActor>("Who? Random extra"),
@@ -19,7 +20,7 @@ MyActorList createList()
   return actors;
 }
 
-void pushToLua(lua_State* L, MyActorList list)
+void pushActorsToLua(lua_State* L, MyActorList list)
 {
   lua_newtable(L);
   int i = 1;
@@ -30,13 +31,9 @@ void pushToLua(lua_State* L, MyActorList list)
   }
 }
 
-MyActorList pullFromLua(lua_State* L)
+MyActorList pullActorsFromLua(lua_State* L)
 {
   // Note this only stores the values, not the keys/indexes.
-  // The keys are available at index -2 after calling lua_next,
-  // but if you need it, copy it first using lua_pushvalue and use that
-  // so that the original is left intact for the next call to lua_next.
-  //
   // Also this simply skips over any elements that are not 'MyActor's.
   MyActorList list;
 
@@ -54,6 +51,54 @@ MyActorList pullFromLua(lua_State* L)
   return list;
 }
 
+using StrStrMap = std::unordered_map<std::string, std::string>;
+
+const StrStrMap& createMap()
+{
+  static StrStrMap map = {{"One", "Uno"},
+                          {"Two", "Dos"},
+                          {"Three", "Tres"},
+                          {"Four", "Quatro"},
+                          {"Five", "Cinco"}};
+  return map;
+}
+
+void pushMap(lua_State* L, const StrStrMap& map)
+{
+  lua_newtable(L);
+  for (const auto& [key, item] : map) {
+    lua_pushstring(L, item.c_str());
+    lua_setfield(L, -2, key.c_str());
+  }
+}
+
+StrStrMap pullMap(lua_State* L)
+{
+  StrStrMap ret;
+
+  if (lua_istable(L, -1)) {
+    lua_pushnil(L);
+    while (lua_next(L, -2)) {
+      // The key is available at index -2 after calling lua_next.
+      // You need to avoid modifing it for the next call to lua_next
+      // to work correctly. This includes avoiding calling lua_to[l]string
+      // when the key is not already a string.
+      // We're just going to check the key is a string and skip if it is not.
+      if (lua_isstring(L, -2)) {
+        std::string key = lua_tostring(L, -2);
+        std::string value = lua_tostring(L, -1);
+
+        ret.emplace(key, value);
+      }
+
+      // Pop the value, leaving the key at the top of the stack for lua_next.
+      lua_pop(L, 1);
+    }
+  }
+
+  return ret;
+}
+
 int main()
 {
   lua_State* L = luaL_newstate();
@@ -61,31 +106,48 @@ int main()
 
   MyActorBinding::register_class(L);
 
+  // Lua table and a vector of class instances.
+  // ------------------------------------------
   {
-    cout << "Pushing actor list to Lua." << endl;
-    MyActorList actors = createList();
-    pushToLua(L, actors);
+    cout << "Pushing actor list to Lua.\n";
+    MyActorList actors = createActorList();
+    pushActorsToLua(L, actors);
     lua_setglobal(L, "actors");
   }
 
   run(L, "for k,v in pairs(actors) do print( k, v.name ) end");
 
-  cout << "Editing actor list..." << endl;
+  cout << "Editing actor list..." << "\n";
   run(L, "actors[2] = nil                    -- Who is this? Pfft, delete.");
   run(L, "collectgarbage()");
   run(L, "actors['test'] = MyActor('Bob')    -- Hey Bob welcome.");
 
-  run(L, "for k,v in pairs(actors) do print( k, v.name ) end");
+  run(L, "for k, v in pairs(actors) do print(k, v.name) end");
 
   {
-    cout << "Pull list back into C++ vector, and list..." << endl;
+    cout << "Pull list back into C++ vector, and list...\n";
     lua_getglobal(L, "actors");
-    MyActorList actors = pullFromLua(L);
+    MyActorList actors = pullActorsFromLua(L);
     lua_pop(L, 1);
 
     for (const auto& actor : actors) {
-      cout << actor->_name << endl;
+      cout << actor->_name << "\n";
     }
+  }
+
+  // Lua table of strings with keys and a C++ map.
+  // ---------------------------------------------
+  StrStrMap map = createMap();
+  pushMap(L, map);
+  lua_setglobal(L, "map");
+
+  run(L, "for k, v in pairs(map) do print('Lua:', k, v) end");
+
+  lua_getglobal(L, "map");
+  StrStrMap ret = pullMap(L);
+
+  for (const auto& [key, value] : ret) {
+    cout << "C++:\t" << key << "\t" << value << "\n";
   }
 
   lua_close(L);
