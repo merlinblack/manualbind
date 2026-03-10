0629545208d55b08d51a18b8a3827184bd7c650f
Nigel Atkinson <merlinblack@users.noreply.github.com>
2026-03-11 10:12:25 +1100

    add lua_close to getluafunc example

--- examples/getluafunc/getluafunc.cpp
+++ examples/getluafunc/getluafunc.cpp
@@ -22,7 +22,7 @@ int main(int argc, char* argv[])
     function nothing()
       print('No args, no return. Bit boring really.')
     end
-  )";
+    )";
 
   lua_State* L = luaL_newstate();
   luaL_openlibs(L);
@@ -34,38 +34,42 @@ int main(int argc, char* argv[])
     return EXIT_FAILURE;
   }
 
-  // std::function<int(int, int)>
-  auto add = getLuaFunc<int, int, int>(L, "add");
+  {
+    // std::function<int(int, int)>
+    auto add = getLuaFunc<int, int, int>(L, "add");
 
-  // std::function<float(float, float)>, but the same Lua function as 'add'.
-  auto addF = getLuaFunc<float, float, float>(L, "add");
+    // std::function<float(float, float)>, but the same Lua function as 'add'.
+    auto addF = getLuaFunc<float, float, float>(L, "add");
 
-  // std::function<string(string, string, string)>
-  auto concat = getLuaFunc<string, string, string, string>(L, "concat");
+    // std::function<string(string, string, string)>
+    auto concat = getLuaFunc<string, string, string, string>(L, "concat");
 
-  // The first template parameter, the return type, defaults to 'void'.
-  // The lack of any template parameters after that gives an empty arg list.
-  // std::function<void()>
-  auto boring = getLuaFunc(L, "nothing");
+    // The first template parameter, the return type, defaults to 'void'.
+    // The lack of any template parameters after that gives an empty arg list.
+    // std::function<void()>
+    auto boring = getLuaFunc(L, "nothing");
 
-  // Any parameters you like (that LuaStack can push). There is no type
-  // checking.
-  // LuaRef
-  auto print = LuaRef::getGlobal(L, "print");
+    // Any parameters you like (that LuaStack can push). There is no type
+    // checking.
+    // LuaRef
+    auto print = LuaRef::getGlobal(L, "print");
 
-  int x = add(5, 10);
-  float y = addF(41.5, 0.5);
+    int x = add(5, 10);
+    float y = addF(41.5, 0.5);
 
-  // Works but first parameter is implicitly narrowed to an int.
-  int z = add(123.2, 10);
+    // Works but first parameter is implicitly narrowed to an int.
+    int z = add(123.2, 10);
 
-  // int nope = add("Hello", "World"); - does not compile, no matching call.
+    // int nope = add("Hello", "World"); - does not compile, no matching call.
 
-  string result = concat("Hello", " ", "world!");
+    string result = concat("Hello", " ", "world!");
 
-  boring();
+    boring();
 
-  print(result, x, y, z);
+    print(result, x, y, z);
+  }
+
+  lua_close(L);
 
   return EXIT_SUCCESS;
 }
